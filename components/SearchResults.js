d85aa8e8a8ce573bcdf7be87225a8be277d7fd07
Anna Jose <joann23july@gmail.com>
2021-10-26 21:24:07 -0400

    updated

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -7,7 +7,7 @@ function SearchResults({results}) {
             {results.items?.map((result) => (
                 <div key={result.link} className="max-w-xl mb-8">
                     <div className="group ">
-                        <a href={result.link} className="text-sm">{result.formattedUrl}</a>
+                        {/* <a href={result.link} className="text-sm truncate break-words">{result.formattedUrl}</a> */}
                         <a href={result.link} ><h2 className="truncate text-xl
                         text-blue-800 font-medium group-hover:underline">{result.title}</h2></a>
                     </div>
